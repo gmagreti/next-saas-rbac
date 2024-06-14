59ab12839bb43857aee96b201d20c0875840cb02
Gabriel Magreti <gabrielmagreti@gmail.com>
2024-06-13 22:17:51 -0300

    feat: (auth) handling user authentication

---
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -0,0 +1,69 @@
+import { compare } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sesstions/password',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with e-mail & password',
+        body: z.object({
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+        response: {
+          201: z.object({
+            token: z.string(),
+          }),
+          400: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        return reply.status(404).send({ message: 'Invalid credentials.' })
+      }
+
+      if (userFromEmail.passwordHash === null) {
+        return reply
+          .status(404)
+          .send({ message: 'User does not have a password, use social login.' })
+      }
+
+      const isValidPassword = await compare(
+        password,
+        userFromEmail.passwordHash,
+      )
+
+      if (!isValidPassword) {
+        return reply.status(404).send({ message: 'Invalid credentials.' })
+      }
+
+      const token = app.jwt.sign(
+        {
+          sub: userFromEmail.id,
+        },
+        {
+          expiresIn: '7d',
+        },
+      )
+
+      return reply.status(201).send({ token })
+    },
+  )
+}
